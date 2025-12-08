73e80782245031b17aef7e110d96a55494c48743
Meh <chasminvr@gmail.com>
2025-12-08 01:09:21 -0600

    Initial commit

---
+++ src/main/java/net/meh/xenon/Xenon.java
@@ -0,0 +1,16 @@
+package net.meh.xenon;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Xenon implements ModInitializer {
+	public static final String MOD_ID = "xenon";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/meh/xenon/XenonClient.java
@@ -0,0 +1,10 @@
+package net.meh.xenon;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class XenonClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
